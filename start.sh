88dcbfabdd4da46405d5b81f35587a49cedf9913
Mad To AzenZio <89691212+madtoazenzio@users.noreply.github.com>
2021-11-22 20:10:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/PredatorHackerzZ/Calculator-Bot.git /Calculator-Bot
+git clone https://github.com/madtoazenzio/Calculator-Bot-1.git
+ /Calculator-Bot
 cd /Calculator-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
